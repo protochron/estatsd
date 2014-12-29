2f86f04d15c628c13ecdbf99a56f994a961f9b9a
Dan Norris <daniel.norris@acquia.com>
2014-12-28 23:57:48 -0500

    Set package metadata

--- mix.exs
+++ mix.exs
@@ -8,23 +8,18 @@ defmodule Estatsd.Mixfile do
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: [:logger, :conform]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     [{:conform, "~> 0.12.0"}]
+  end
+
+  defp package do
+    [contributers: ["Dan Norris"],
+     licenses: ["Apache"]
+    ]
+
   end
 end
